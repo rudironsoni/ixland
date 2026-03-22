40f24923d626fa6b43668a6240a2e147c9ae750b
Rudimar Ronsoni <rudimar@outlook.com>
2026-03-22 12:25:36 +0100

    chore: Remove build directory from git tracking

--- build/DerivedData/ios/Build/Intermediates.noindex/a_shell_system.build/Release-iphoneos/a_shell_system.build/Objects-normal/arm64/a_shell_system_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/rudironsoni/src/github/rudironsoni/a-shell-next/build/DerivedData/ios/Build/Intermediates.noindex/a_shell_system.build/Release-iphoneos/a_shell_system.build/DerivedSources/a_shell_system_vers.c
