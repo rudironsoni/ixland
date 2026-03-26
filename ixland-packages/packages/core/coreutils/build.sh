01a99c7cba029d7fceffebcd92a24638c7a90d9e
Rudimar Ronsoni <rudimar@outlook.com>
2026-03-26 16:42:10 +0100

    Fix: Import correct packages repository into ixland-packages (#4)

---
+++ ixland-packages/packages/core/coreutils/build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# build.sh - Build recipe for GNU coreutils
+
+A_SHELL_PKG_NAME="coreutils"
+A_SHELL_PKG_VERSION="9.5"
+A_SHELL_PKG_SRCURL="https://ftp.gnu.org/gnu/coreutils/coreutils-9.5.tar.gz"
+A_SHELL_PKG_SHA256=""
+A_SHELL_PKG_DEPENDS=""
+A_SHELL_PKG_DESCRIPTION="GNU Core Utilities (ls, cp, mv, cat, etc.)"
+
+a_shell_pkg_configure() {
+    # Cache variables for cross-compilation
+    export ac_cv_sizeof_long=8
+    export ac_cv_sizeof_size_t=8
+    export ac_cv_sizeof_ssize_t=8
+    export ac_cv_sizeof_off_t=8
+    export ac_cv_sizeof_long_long=8
+    export ac_cv_sizeof_dev_t=4
+    export ac_cv_sizeof_ino_t=8
+    export ac_cv_func_getcwd_null=yes
+    export ac_cv_func_getgroups_works=yes
+    export ac_cv_func_memcmp_working=yes
+    export ac_cv_func_strtod_works=yes
+    export ac_cv_func_chown_works=yes
+    export ac_cv_func_fchmodat_works=yes
+    export ac_cv_func_fchownat_works=yes
+    export ac_cv_func_lchmod_works=no
+    export ac_cv_func_stat_empty_string_bug=no
+    export ac_cv_func_working_mktime=yes
+    export ac_cv_func_fnmatch_works=yes
+    export ac_cv_func_fnmatch_gnu=yes
+    export gl_cv_func_gettimeofday_clobber=no
+    export gl_cv_func_tzset_clobber=no
+    
+    # iOS doesn't have clock_settime
+    export ac_cv_func_clock_settime=no
+    export gl_cv_func_clock_settime=no
+    
+    # Disable programs that require root or special permissions
+    ./configure \
+        --prefix="$A_SHELL_PREFIX" \
+        --host="arm-apple-darwin" \
+        --disable-nls \
+        --disable-rpath \
+        --disable-libcap \
+        --disable-xattr \
+        --disable-libsmack \
+        --without-selinux \
+        --without-gmp \
+        --enable-install-program="arch,hostname" \
+        || a_shell_error "Configure failed"
+}
+
+a_shell_pkg_make() {
+    make -j$(sysctl -n hw.ncpu) || a_shell_error "Build failed"
+}
+
+a_shell_pkg_install() {
+    make install DESTDIR="$A_SHELL_PKG_STAGING" || a_shell_error "Install failed"
+}
---
+++ ixland-packages/packages/core/readline/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# build.sh - Build recipe for GNU readline
+
+A_SHELL_PKG_NAME="readline"
+A_SHELL_PKG_VERSION="8.2"
+A_SHELL_PKG_SRCURL="https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz"
+A_SHELL_PKG_SHA256="3feb7171f16a84ee82ca18a36d7b9be109a52c04f492a053331d7d1095007c35"
+A_SHELL_PKG_DEPENDS="ncurses"
+A_SHELL_PKG_DESCRIPTION="GNU readline library"
+
+a_shell_pkg_configure() {
+    # iOS-specific settings
+    export ac_cv_func_getentropy=no
+    export ac_cv_lib_ncursesw=yes
+    export ac_cv_lib_ncurses=yes
+    
+    # Configure with iOS paths
+    ./configure \
+        --prefix="$A_SHELL_PREFIX" \
+        --host="arm-apple-darwin" \
+        --disable-shared \
+        --enable-static \
+        --with-curses \
+        --without-purify \
+        bash_cv_termcap_lib=libncurses \
+        || a_shell_error "Configure failed"
+}
+
+a_shell_pkg_make() {
+    make -j$(sysctl -n hw.ncpu) || a_shell_error "Build failed"
+}
+
+a_shell_pkg_install() {
+    make DESTDIR="$A_SHELL_PKG_STAGING" install || a_shell_error "Install failed"
+}
